1917ff04faa45e1cec3f22610d6fb37e6bf99ebf
zxul767 <willebaldo.gomez@gmail.com>
2019-11-15 00:05:29 -0600

    Fix linting error

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Travis!\n')
+  response.end('Hello Travis!\n');
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
